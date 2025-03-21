fc5226089d4d4afe686cc320f8e392cbfe676851
Alt Mdz <test@ansible.net>
2025-03-21 16:04:41 -0600

    Se puede utilizar en servidor real

--- bash_scripts/diskspace_alert.sh
+++ bash_scripts/diskspace_alert.sh
@@ -1,33 +1,24 @@
-!/bin/sh
+#!/bin/sh
 # set -x
 # Shell script to monitor or watch the disk space
-# It will send an email to $ADMIN, if the (free available) percentage of space is >= 90%.
+# It will redirect to a FIFO file if percentage of space is >= 10% for testing
 # --------------------------------------------------------------------------------------------------------
-# Set admin email so that you can get email.
-ADMIN="root"
-# set alert level 90% is default
-ALERT=90
-# Exclude list of unwanted monitoring, if several partions then use "|" to separate the partitions.
-# An example: EXCLUDE_LIST="/dev/hdd1|/dev/hdc5"
-EXCLUDE_LIST="/auto/ripper|loop"
+
+LOG_FILE="/phat/to/fifo_file.txt"
+DATE=$(date "+%Y-%m-%d %H:%M:%S")
 #
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#
-main_prog() {
-while read -r output;
-do
-  #echo "Working on $output ..."
-  usep=$(echo "$output" | awk '{ print $1}' | cut -d'%' -f1)
-  partition=$(echo "$output" | awk '{print $2}')
-  if [ $usep -ge $ALERT ] ; then
-     echo "Running out of space \"$partition ($usep%)\" on server $(hostname), $(date)" | \
-     echo mail -s "Alert: Almost out of disk space $usep% on $partition" $ADMIN
-  fi
-done
-}
- 
-if [ "$EXCLUDE_LIST" != "" ] ; then
-  df -H | grep -vE "^Filesystem|tmpfs|cdrom|${EXCLUDE_LIST}" | awk '{print $5 " " $6}' | main_prog
-else
-  df -H | grep -vE "^Filesystem|tmpfs|cdrom" | awk '{print $5 " " $6}' | main_prog
+#he pattern /[0-9]+%/ ensures that we are selecting lines where $5 contains numbers followed by % (to avoid headers).
+df -h | awk '$5 ~ /[0-9]+%/ {gsub("%", "", $5); if ($5 >= 10) print $0}' > /tmp/disk_alert.tmp
+
+# If any partition is above 10%, log it this is for testing change it
+if [[ -s /tmp/disk_alert.tmp ]] 
+then
+    echo "[$DATE] ALERT: High disk usage detected! on server $(hostname)" >> $LOG_FILE
+    cat /tmp/disk_alert.tmp >> $LOG_FILE
+    echo "----------------------------" >> $LOG_FILE
 fi
+
+# Cleanup temporary file
+rm -f /tmp/disk_alert.tmp
+
